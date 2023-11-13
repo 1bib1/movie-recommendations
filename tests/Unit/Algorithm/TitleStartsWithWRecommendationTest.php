9274e6712366c97f063128fc2e1c1eaeba7e2c5d
bargrz-cod <135323550+bargrz-cod@users.noreply.github.com>
2023-11-13 19:31:41 +0100

    add movie test for title start with "w" algorithm

--- tests/Unit/Algorithm/TitleStartsWithWRecommendationTest.php
+++ tests/Unit/Algorithm/TitleStartsWithWRecommendationTest.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Tests\Algorithm;
 
 use App\RecommendationAlgorithm\TitleStartWithWRecommendation;
+use App\Util\Const\Movies;
 use App\Util\Generator\NonRepetitiveRandomNumberGenerator;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Asset\Package;
 
 final class TitleStartsWithWRecommendationTest extends TestCase
 {
@@ -23,9 +25,8 @@ final class TitleStartsWithWRecommendationTest extends TestCase
     /**
      * @dataProvider getRecommendationsDataProvider
      */
-    public function testGetRecommendations(array $numbersToReturn, array $allMovies, array $expectedMovies): void
+    public function testGetRecommendations(int $numberOfMoviesToRecommend, array $numbersToReturn, array $allMovies, array $expectedMovies): void
     {
-        $numberOfMoviesToRecommend = 3;
         $this->randomNumberGenerator->method('generate')->willReturn($numbersToReturn);
 
         $recommendedMovies = $this->algorithm->getRecommendations($numberOfMoviesToRecommend, $allMovies);
@@ -36,10 +37,37 @@ final class TitleStartsWithWRecommendationTest extends TestCase
     public static function getRecommendationsDataProvider(): array
     {
         return [
-            'number_of_recommendations_is_same_length_as_array' => [[], ['W0', 'w', 'W1', 'w', 'W2'], ['W0', 'W1', 'W2']],
-            'different_number_of_recommendations' => [[0,1,4], ['Incepcja', 'W0', 'w1', 'Shrek', 'w4' , 'Something'], ['W0', 'w1', 'w4']],
+            'number_of_recommendations_is_same_length_as_array' => [
+                3,
+                [], // should return without randomizing
+                ['W0', 'w', 'W1', 'w', 'W2'],
+                ['W0', 'W1', 'W2']],
+            'different_number_of_recommendations' => [
+                3,
+                [0, 1, 4],
+                ['Incepcja', 'W0', 'w1', 'Shrek', 'w4' , 'Something'],
+                ['W0', 'w1', 'w4']
+            ],
+            'movie_recommendations' => [
+                3,
+                [],  // should return without randomizing, because there are only 3 matching movie titles
+                Movies::MOVIES,
+                [
+                    'Whiplash',
+                    'Wyspa tajemnic',
+                    'Władca Pierścieni: Drużyna Pierścienia',
+                ],
+            ],
+            '3_random_movies' => [
+                3,
+                [0, 1, 3],
+                array_merge(Movies::MOVIES, ['Wonder Woman']),
+                [
+                    'Whiplash',
+                    'Wyspa tajemnic',
+                    'Wonder Woman',
+                ],
+            ],
         ];
     }
-
-    // TODO: add
 }
